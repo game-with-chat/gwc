77072af93e9c4f01ce1885e2518efbcaf86ea46e
Tumble <tumblegamer@gmail.com>
2024-04-18 18:39:15 +0100

    no more pecking

--- Assets/MVP/PointClickControler.cs
+++ Assets/MVP/PointClickControler.cs
@@ -1,28 +1,27 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Player : MonoBehaviour {
 	public string username = "p0";
 
-	public GameObject appearence;
 	public GameObject nametag;
 
 	private void Start() {
-		nametag.GetComponent<	TextMesh>().text = username;
+		nametag.GetComponent<TextMesh>().text = username;
 	}
 
 	private void Update() {
 		if(Input.GetButtonDown("GoHere")) {
-			Debug.Log("Mouse clicked");
 			Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
 			RaycastHit mouseClickImpact;
 			if(Physics.Raycast(mouseRay,out mouseClickImpact)) {
-				Debug.Log("mouse impact");
 				Vector3 newPosition = mouseClickImpact.point;
+				newPosition.y = transform.position.y;
 				//transform.position = newPosition;
-				 GetComponent<UnityEngine.AI.NavMeshAgent>().SetDestination(newPosition);
+				 GetComponent<NavMeshAgent>().SetDestination(newPosition);
 
 				 Vector3 direction = (newPosition - transform.position).normalized;
-				appearence.rotation = Quaternion.LookRotation(direction);
+				transform.rotation = Quaternion.LookRotation(direction);
 			}
 		}
 	}
