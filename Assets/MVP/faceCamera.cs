79e7ccf83ef082c5bed758f39536616426633589
Tumble <tumblegamer@gmail.com>
2024-04-19 12:45:42 +0100

    Added nice spaces to the '-' and maybe it is reaable, question mark

--- Assets/MVP/faceCamera.cs
+++ Assets/MVP/faceCamera.cs
@@ -7,7 +7,7 @@ public class alwaysFaceCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		Vector3 direction = Camera.main.transform.position-transform.position;
+		Vector3 direction = Camera.main.transform.position - transform.position;
 		if(reverse) direction *=-1;
 		if(stayUpright) direction.x = 0;
         transform.rotation = Quaternion.LookRotation(direction);
