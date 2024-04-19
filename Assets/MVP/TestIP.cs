420721bb4019cd67ac2ecaef9d489a430b905e15
Tumble <tumblegamer@gmail.com>
2024-04-19 18:08:24 +0100

    an ip test thing

---
+++ Assets/MVP/TestIP.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using FishNet.Component.Spawning;
+using FishNet.Managing;
+using FishNet.Managing.Observing;
+using FishNet.Object;
+using FishNet.Transporting;
+using Unity.VisualScripting;
+using UnityEngine;
+
+[RequireComponent(typeof(NetworkManager))]
+public class TestIP : MonoBehaviour
+{
+
+	public string ip = "localhost";
+	public ushort port = 7770;
+	public bool hostServer = false;
+
+	private NetworkManager networkManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        networkManager = GetComponent<NetworkManager>();
+
+		Transport transport = networkManager.TransportManager.Transport;
+		transport.SetClientAddress(ip);
+		transport.SetPort(port);
+		transport.SetServerBindAddress(ip,IPAddressType.IPv4);
+
+		if(hostServer) {
+			networkManager.ServerManager.StartConnection(port);
+		} if(ip!="localhost") {
+			networkManager.ClientManager.StartConnection(ip,port);
+		}
+
+		Debug.Log($"Address: {transport.GetClientAddress()}");
+		Debug.Log($"Port: {transport.GetPort()}");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
