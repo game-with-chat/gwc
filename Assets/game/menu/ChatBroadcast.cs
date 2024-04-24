69ce2bf2a92996c5dfd753e9a66db7bfdb35bb06
Tumble <tumblegamer@gmail.com>
2024-04-24 01:00:09 +0100

    fixed some of the chat stuff

--- Assets/game/menu/ChatBroadcast.cs
+++ Assets/game/menu/ChatBroadcast.cs
@@ -1,6 +1,7 @@
 using FishNet;
 using FishNet.Broadcast;
 using FishNet.Connection;
+using FishNet.Object;
 using JamesFrowen.SimpleWeb;
 using System.Collections;
 using System.Collections.Generic;
@@ -8,26 +9,37 @@ using TMPro;
 using UnityEngine;
 
 
-public class ChatBroadcast : MonoBehaviour
+public class ChatBroadcast : NetworkBehaviour
 {
-	public Transform chatHolder;
-	public GameObject msgElement;
-	public TMP_InputField playerUsername, playerMessage;
+	// public Transform chatHolder;
+	// public GameObject msgElement;
+	public TMP_InputField playerMessage;
 
-	private void Update()
+	public override void OnStartClient()
 	{
-		//If owner and space bar is pressed.
-		if (base.IsOwner && Input.GetKeyDown(KeyCode.Space))
-			RpcSendChat(playerMessage);
+		base.OnStartClient();
 	}
 
-	[ServerRpc]
-	private void RpcSendChat(string msg)
+	// private void Update()
+	// {
+	// 	//If owner and space bar is pressed.
+	// 	if (base.IsOwner && Input.GetKeyDown(KeyCode.Space))
+	// 		RpcSendChat(playerMessage.text);
+	// }
+
+	public void SEND_CHAT() {
+		RpcSendChat(playerMessage.text);
+	}
+
+	[ServerRpc(RequireOwnership =false)]
+	private void RpcSendChat(string msg, NetworkConnection connection=null)
 	{
 
 		// need to update the chatlog box with the message
+
+		string name = UsernameManager.GetUsername(connection.ClientId);
 		
-		Debug.Log($"Received {msg} on the server.");
+		Debug.Log($"Received {msg} on the server by ${name}");
 	}
 }
 
